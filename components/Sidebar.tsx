428f21b62c1fecdbdb256643328d11b8575d223e
Kelvin Achi <kellywizy09@gmail.com>
2026-01-02 16:05:33 +0100

    feat: add SVG icons and implement Navbar and Sidebar components

---
+++ components/Sidebar.tsx
@@ -0,0 +1,15 @@
+import { House, Mail, Bell, User, SquarePen } from "lucide-react";
+
+const Sidebar = () => {
+  return (
+    <div>
+      <House />
+      <Mail />
+      <Bell />
+      <User />
+      <SquarePen />
+    </div>
+  )
+}
+
+export default Sidebar
