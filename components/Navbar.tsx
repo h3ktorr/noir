9a6feadf2020888b0878233e6f12410948cf5bff
Kelvin Achi <kellywizy09@gmail.com>
2026-01-02 17:11:58 +0100

    Remove unused SVG assets and update Navbar and Sidebar components for improved layout and responsiveness

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,23 @@
+import Image from "next/image"
+
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className="flex items-center justify-between h-24 w-[calc(100vw-6rem)] border-b border-foreground p-8">
+      <Image 
+        src="/light-logo.svg"
+        alt="Logo"
+        width={100}
+        height={100}
+        className="dark:hidden"
+      />
+      <Image 
+        src="/dark-logo.svg"
+        alt="Logo"
+        width={100}
+        height={100}
+        className="hidden dark:block"
+      />
+      <input type="text" placeholder="Search..." className="px-4 py-2 w-72 rounded-md bg-background border border-foreground focus:outline-none focus:ring-2 focus:ring-primary" />
     </div>
   )
 }
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,12 +2,12 @@ import { House, Mail, Bell, User, SquarePen } from "lucide-react";
 
 const Sidebar = () => {
   return (
-    <div>
-      <House />
-      <Mail />
-      <Bell />
-      <User />
-      <SquarePen />
+    <div className="flex flex-col border-r border-foreground w-24 h-screen text-foreground gap-12 items-center justify-center">
+      <House width={45} height={45} fill="currentColor" stroke="none" />
+      <Mail width={45} height={45} />
+      <Bell width={45} height={45} />
+      <User width={45} height={45} />
+      <SquarePen width={45} height={45} />
     </div>
   )
 }
