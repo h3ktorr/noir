a3fac607835d177ae4fbef9c1cc97b92b7c5d717
Kelvin Achi <kellywizy09@gmail.com>
2026-01-10 19:45:09 +0100

    feat: enhance notifications layout with improved styling and cursor pointer for tabs

--- app/notifications/page.tsx
+++ app/notifications/page.tsx
@@ -5,9 +5,9 @@ const page = () => {
   return (
     <div className="p-4">
       <h1 className="text-2xl font-bold">Notifications</h1>
-      <div className="mt-4 flex space-x-4 border-b border-gray-300 pb-2">
-        <p>All</p>
-        <p>Unread</p>
+      <div className="mt-4 flex space-x-6 border-b border-gray-300 pb-2 font-semibold">
+        <p className="cursor-pointer">All</p>
+        <p className="cursor-pointer">Unread</p>
       </div>
       <div className="">
         {
@@ -16,8 +16,8 @@ const page = () => {
               <div className="flex items-center space-x-3">
                 <Image src={notification.image} alt={notification.username} className="w-10 h-10 rounded-full" />
                 <div>
-                  <p className="font-semibold">{notification.username}</p>
-                  <p className="text-sm text-gray-600">{notification.text}</p>
+                  <p className="font-semibold">{notification.username} <span className="text-sm text-gray-400 ml-2">{notification.text}</span></p>
+                  
                 </div>
               </div>
             </div>
