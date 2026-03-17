ec6fddd8015113045017943772c05c3ec931abfb
Kelvin Achi <kellywizy09@gmail.com>
2026-03-17 12:16:08 +0100

    fix: update Prisma client import and configuration for consistency

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,7 @@
-import { PrismaClient } from "../app/generated/prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../app/generated/prisma";
 const globalForPrisma = global as unknown as {
   prisma: PrismaClient;
 };
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
-});
-const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    adapter,
-  });
+const prisma = globalForPrisma.prisma || new PrismaClient();
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 export default prisma;
